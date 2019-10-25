94470871327e4b6c93d733271b0a970d1d4f0aee
LuisaZ <luisaziccarelli@gmail.com>
2019-10-25 15:34:24 +1100

    week 1 stuff

---
+++ week1/bartender-challenge.rb
@@ -0,0 +1,25 @@
+
+
+cocktail= 3
+water=2
+beer=6
+
+puts "Hi there, what would you like to drink?"
+
+order= gets   
+
+if order= "cocktail"
+#{cocktail} + 1 
+elseif order= "water"
+    #{water} + 1
+elseif order= "beer"
+    #{beer} + 1
+end
+
+order_total= 
+profits= [cocktails=> 14 , water=> 9 , beer= 5.85]
+
+
+
+
+
