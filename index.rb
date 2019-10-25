491c28d2045dfe99d72c328537c01f67629b2418
LuisaZ <luisaziccarelli@gmail.com>
2019-10-25 12:29:38 +1100

    conditionals practice

---
+++ index.rb
@@ -0,0 +1,8 @@
+# create conditions
+    # check wether someone should get entry
+        # check if drunk
+            # reject if drunk  
+            # allow if not drunk 
+        # check if under 18 
+            # reject if under 18 
+            # allow if over 18 
