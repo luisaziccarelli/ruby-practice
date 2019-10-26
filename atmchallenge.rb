4dc3758521e4368fb14d69e059c7259a701b6c66
LuisaZ <luisaziccarelli@gmail.com>
2019-10-26 16:48:43 +1100

    with names

--- atmchallenge.rb
+++ atmchallenge.rb
@@ -1,41 +1,40 @@
 ## atm functions 
 #
 # essential features
-user id
-pin entry
-check balance
-withdraw money 
+# user = id
+# pin entry
+# check balance
+# withdraw money 
 
 # nice to have features 
-coin deposit
-check deposit
-receipt 
-curreny exchange
+# coin deposit
+# check deposit
+# receipt 
+# curreny exchange
 
-# 
+user_id = "luisa"
+#user_id = true
 
-user_id = user_id_in_system
-user_id = true 
-pin =  pin_in_system 
-pin = false 
+pin = "ziccarelli"
+#pin = true 
 
+puts "Hi, please enter your user id. Your user id is usually your first name"
+user_id = gets.chomp.to_s
 
-puts "Hi, please enter your user id"
-user_id= gets.chomp.to_s
-
-
-puts "Hi, please enter your pin. It should contain 4 digits"
-pin= gets.chomp.to_s
-
-
-if user_id = user_id_in_system
-    puts "Great, what would you like to do today. Select 1 for check your balance, Select 2 for withdrawal"
-    elsif user_id != user_id_in_system
-    puts "Sorry, we can't find you in our system. Check you have entered the correct details"
-    else "Function not detected, please try again"
+if user_id == "Amy"
+    puts "Great, please enter your pin. Your pin is usually your last name"
+    pin = gets.chomp.to_s
+    if pin == "last name"
+        puts "Welcome!"
+    end 
+else 
+    puts "Sorry your user id does not match"
 end 
-
-
-
+# ##
+# elsif pin == true 
+# puts "Great, what would you like to do today. Select 1 for check your balance, Select 2 for withdrawal" 
+# else pin == false
+# puts "Sorry your user id does not match"
+# end
 
 
