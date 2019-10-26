1265d6a711ac1be0715606b464f49526bb14fbab
LuisaZ <luisaziccarelli@gmail.com>
2019-10-26 15:37:14 +1100

    atm atm

---
+++ atmchallenge.rb
@@ -0,0 +1,17 @@
+## atm functions 
+
+#
+# essential features
+user id
+pin entry
+check balance
+withdraw money 
+
+#
+# nice to have features 
+coin deposit
+check deposit
+receipt 
+curreny exchange
+
+
