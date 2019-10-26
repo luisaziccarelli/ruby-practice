323b2a334a54e040697042b7b9e0428242e8b3a2
LuisaZ <luisaziccarelli@gmail.com>
2019-10-26 15:55:44 +1100

    brainstorming

--- atmchallenge.rb
+++ atmchallenge.rb
@@ -1,5 +1,4 @@
 ## atm functions 
-
 #
 # essential features
 user id
@@ -7,11 +6,36 @@ pin entry
 check balance
 withdraw money 
 
-#
 # nice to have features 
 coin deposit
 check deposit
 receipt 
 curreny exchange
+
+# 
+
+user_id = user_id_in_system
+user_id = true 
+pin =  pin_in_system 
+pin = false 
+
+
+puts "Hi, please enter your user id"
+user_id= gets.chomp.to_s
+
+
+puts "Hi, please enter your pin. It should contain 4 digits"
+pin= gets.chomp.to_s
+
+
+if user_id = user_id_in_system
+    puts "Great, what would you like to do today. Select 1 for check your balance, Select 2 for withdrawal"
+    elsif user_id != user_id_in_system
+    puts "Sorry, we can't find you in our system. Check you have entered the correct details"
+    else "Function not detected, please try again"
+end 
+
+
+
 
 
