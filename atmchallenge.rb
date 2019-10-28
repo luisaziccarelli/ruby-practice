4f1515c978c29a5ae9c4135d25fa31451e688e2c
LuisaZ <luisaziccarelli@gmail.com>
2019-10-28 22:16:38 +1100

    id and password

--- atmchallenge.rb
+++ atmchallenge.rb
@@ -13,28 +13,22 @@
 # curreny exchange
 
 user_id = "luisa"
-#user_id = true
 
 pin = "0000"
-#pin = true 
 
 puts "Hi, please enter your user id. Your user id is usually your first name"
 user_id = gets.chomp.to_s
 
 if user_id == "luisa"
-    puts "Great, please enter your pin. Your pin is usually your last name"
+    puts "Great, please enter your pin. Your pin is usually 4 digits"
     pin = gets.chomp.to_s
     if pin == "0000"
-        puts "Welcome!"
+        puts "Welcome to your account"
+    elsif pin != "0000"
+        puts "Wrong pin, sorry. Try again!"
     end 
 else 
     puts "Sorry your user id does not match"
 end 
-# ##
-# elsif pin == true 
-# puts "Great, what would you like to do today. Select 1 for check your balance, Select 2 for withdrawal" 
-# else pin == false
-# puts "Sorry your user id does not match"
-# end
 
 
