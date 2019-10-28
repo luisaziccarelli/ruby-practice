f51dd265dc984109a1c25e3ca20e4a505ec57a76
LuisaZ <luisaziccarelli@gmail.com>
2019-10-29 09:59:26 +1100

    . . .

--- first_second_array.rb
+++ first_second_array.rb
@@ -1,8 +1,20 @@
-first =  ["Tommy", "Fred", "Bob", "Charlie"]
-second = ["Tony", "Rhonda" ]
+#first =  ["Tommy", "Fred", "Bob", "Charlie"]
+#second = ["Tony", "Rhonda" ]
 
 #second[2] = "Charlie"
 #first.pop
 #first.reverse!
 
-puts first.include?('Fred')
+#puts first.include?('Fred')
+#puts first.sample(2)
+
+array_integers = [ 1, 2, 3, 3, 2, 1, 7 , 8]
+
+puts array_integers.sum 
+
+#def sum(array_integers)
+
+ #   while array.length > 0
+  #  puts “#{array[x+y+z]}”
+   # else
+    # puts “0”
