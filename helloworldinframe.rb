7ceb862efdc1a60b82e51a0a2ec75a95070953bc
LuisaZ <luisaziccarelli@gmail.com>
2019-10-28 23:19:28 +1100

    frame 2

--- helloworldinframe.rb
+++ helloworldinframe.rb
@@ -1,7 +1,9 @@
-my_array = ["* Hello *", "* World *", "*  in   *", "*  a    *", "* frame *"] 
+my_array = ["Hello", "World", "in", "a", "frame"] 
 
+until my_array.size == 5
+    puts "*" + my_array + "*"
+
+end
 
 puts my_array
-
-
 
