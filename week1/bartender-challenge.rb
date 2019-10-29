124256e114c522eeb93a746f4a29898550359635
LuisaZ <luisaziccarelli@gmail.com>
2019-10-29 22:17:50 +1100

    first one

--- week1/bartender-challenge.rb
+++ week1/bartender-challenge.rb
@@ -1,24 +1,42 @@
 #bartender challenge 
 
-puts "Hi there, what would you like to order? Puts 1 for cocktail, puts 2 for water, puts 3 for beer"
-order= gets.chomp.to_s 
+# orders
+cocktail_count = 3
+water_count = 2
+beer_count = 6
 
-cocktail= 3
-water= 2
-beer= 6
+#profit
+cocktail_profit = 14
+water_profit = 5.85 
+beer_profit = 9 
+
+puts "Hi there, what would you like to order? type cocktail, water or beer"
+order = gets.chomp 
+
+puts "How many would you like?"
+ordernum = gets.to_i
 
 
 if order == "cocktail"
-cocktail + 1 
-elsif order= "water"
-water + 1
-else order= "beer"
-beer + 1
+    cocktail_count += ordernum 
+elsif order == "water"
+    water_count += ordernum
+elsif order == "beer"
+    beer_count += ordernum
+else
+    puts "Sorry, try again, we do not recognise this order!"
 end
 
+total_order = cocktail_count + beer_count + water_count
+puts "Total drinks to make #{total_order}"
 
 
+total_profit = 14 * cocktail_count + 9 * beer_count + 5.85 * water_count
 
+puts cocktail_count
+puts water_count
+puts beer_count
+puts "Total profit #{total_profit}"
 
 
 
