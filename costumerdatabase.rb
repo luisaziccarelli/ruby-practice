237c28a3a068226638bc8773a730212294b78c99
LuisaZ <luisaziccarelli@gmail.com>
2019-10-31 00:58:06 +1100

    draft

---
+++ costumerdatabase.rb
@@ -0,0 +1,21 @@
+# Costumer Database
+
+puts "What is your name?"
+first_name= gets.chomp.to_s
+
+puts "Whar is your last name?"
+last_name= gets.chomp.to_s
+
+puts "#{first_name}\n" + "#{last_name}\n"
+
+#puts "#{first_name}" + "#{last_name}" * 10 
+
+# "Here  is your name times 10"
+
+#store informaiton in an array 
+
+#costumers = [ user_1 , user_2 , ]
+
+#print "Welcome #{first_name} + #{last_name}!"
+#modify above code with option for user to make data entry and option to exit 
+#loop 
