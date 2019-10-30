bf3bbbf245db48e7eb639707e96e3af8859e5708
LuisaZ <luisaziccarelli@gmail.com>
2019-10-31 00:37:58 +1100

    zip

---
+++ booklist.rb
@@ -0,0 +1,10 @@
+#Booklist
+
+favorite_books = [ "the art of happiness" , "siddhartha" , "the little book of love" ]
+
+authors = ["dalai lama" , "herman hesse" , "rumi" ]
+
+puts favorite_books.zip(authors) 
+
+
+
